2f75108694570434a2808515734ebddb75045a36
GentleCold <1952173800@qq.com>
2022-06-04 17:39:54 +0800

    gif加载

--- js/game-engine/game.js
+++ js/game-engine/game.js
@@ -1,12 +1,12 @@
 class Stage {
 
-    #items = [];
+    items = [];
 
     settings = {
         backgroundColor: '#FFFFFF',
         width: 0,
         height: 0,
-        frames: 30, // 1s 30 帧
+        frames: 60, // 1s 60 帧
         draw: function (ctx) {
             ctx.fillStyle = this.backgroundColor;
             ctx.fillRect(0, 0, this.width, this.height);
@@ -20,7 +20,7 @@ class Stage {
     #update(ctx) {
         ctx.clearRect(0, 0, this.settings.width, this.settings.height);
         this.settings.draw(ctx);
-        this.#items.map((item) => {
+        this.items.map((item) => {
             item.update(ctx);
         })
     }
@@ -29,15 +29,17 @@ class Stage {
 
     createItem(settings) {
         const item = new Item(settings);
-        this.#items.push(item);
+        this.items.push(item);
         return item;
     }
 }
 
 class Item {
-
+    #loop = 0; // 当前帧
+    #gifIndex = 0; // gif索引
     #times = 0; // 内部计时器
-
+    #ifGif = false;
+    #ifMove = false;
     settings = {
         x: 0,
         y: 0,
@@ -45,19 +47,11 @@ class Item {
         height: 50,
         color: '#FFFFFF',
         direction: 1, // 0 ~ 3 顺时针
-        speed: 1, // n 帧 update 一次, auto = false 则失效
-        auto: false, // 是否自动变化
-        animate: function () {
-            const offset = [
-                [0, -1],
-                [1, 0],
-                [0, 1],
-                [-1, 0]
-            ];
-
-            this.x += offset[this.direction][0];
-            this.y += offset[this.direction][1];
-        },
+        speed: 15,
+        gap: 1,
+        src: null,
+        gifs: null,
+        loopAnima: function () {},
         draw: function (ctx) {
             ctx.fillStyle = this.color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -69,37 +63,79 @@ class Item {
     }
 
     update(ctx) {
-        if (this.settings.auto) {
+        this.settings.loopAnima();
+        if (this.#ifMove) this.#move();
+        if (this.#ifGif && this.settings.gifs) {
+            ctx.drawImage(this.settings.gifs[this.#gifIndex][this.#loop], this.settings.x, this.settings.y, this.settings.width, this.settings.height);
             if (this.#times === 0) {
-                this.settings.animate();
-                this.#times = (this.#times + 1) % this.settings.speed;
+                this.#loop = (this.#loop + 1) % this.settings.gifs[this.#gifIndex].length;
             }
+            this.#times = (this.#times + 1) % this.settings.gap;
+        } else if (this.settings.src) {
+            ctx.drawImage(this.settings.src, this.settings.x, this.settings.y, this.settings.width, this.settings.height);
+        } else {
+            this.settings.draw(ctx);
         }
-        this.settings.draw(ctx);
     }
 
+    bind(event, callback) {
+        const func = callback.bind(this);
+        document.body.addEventListener(event, func);
+    }
+
+    turnGif(n) {
+        this.#gifIndex = n;
+    }
+
+    stopGif() {
+        this.#ifGif = false;
+    }
+
+    startGif() {
+        this.#ifGif = true;
+    }
+
+    #move() {
+        const offset = [
+            [0, -this.settings.speed],
+            [this.settings.speed, 0],
+            [0, this.settings.speed],
+            [-this.settings.speed, 0]
+        ];
+        this.settings.x += offset[this.settings.direction][0];
+        this.settings.y += offset[this.settings.direction][1];
+    }
+
+    startMove() {
+        this.#ifMove = true;
+    }
+
+    stopMove() {
+        this.#ifMove = false;
+    }
 }
 
 export class Game {
 
+    #loopId;
     #ctx;
-    #width;
-    #height;
+    width;
+    height;
 
     #stages = [];
     #currentStage = 0;
 
     constructor(canvas) {
         this.#ctx = canvas.getContext('2d');
-        this.#width = canvas.width;
-        this.#height = canvas.height;
+        this.width = canvas.width;
+        this.height = canvas.height;
     }
 
     createStage(settings = {}) {
         const stage = new Stage({
             ...{
-                width: this.#width,
-                height: this.#height,
+                width: this.width,
+                height: this.height,
             },
             ...settings,
         });
@@ -113,6 +149,45 @@ export class Game {
     start() {
         const stage = this.#stages[this.#currentStage];
         const ctx = this.#ctx;
-        setInterval(stage.update, 1000 / stage.settings.frames, ctx);
+        this.#loopId = setInterval(stage.update, 1000 / stage.settings.frames, ctx);
+    }
+
+    init() {
+        const stage = this.#stages[this.#currentStage];
+        stage.items.map((item) => {
+            if (item.settings.src) {
+                const img = new Image();
+                img.src = item.settings.src;
+                item.settings.src = img;
+            }
+            if (item.settings.gifs) {
+                item.settings.gifs.map((gif, i) => {
+                    let images = [];
+                    for (let i = 1; i <= gif[1]; i++) {
+                        const img = new Image();
+                        img.src = gif[0] + i + gif[2];
+                        images.push(img);
+                    }
+
+                    item.settings.gifs[i] = images;
+                })
+            }
+        });
+        window.onload = () => this.start();
+    }
+
+
+    pause() {
+        if (this.#loopId) {
+            clearInterval(this.#loopId);
+            this.#loopId = 0;
+        } else {
+            this.start();
+        }
+    }
+
+    bind(event, callback) {
+        const func = callback.bind(this);
+        document.body.addEventListener(event, func);
     }
 }
--- js/index.js
+++ js/index.js
@@ -4,15 +4,73 @@ const canvas = document.getElementById('game');
 
 let game = new Game(canvas);
 let stage = game.createStage({
-    frames: 120,
+    frames: 60,
     backgroundColor: '#4d816a',
 });
-stage.createItem({
+
+game.bind('keydown', function (e) {
+    e.preventDefault();
+    if (e.keyCode === 32) {
+        game.pause();
+    }
+});
+
+const p1 = stage.createItem({
     color: '#fc8383',
-    x: 10,
-    y: 10,
+    x: game.width / 2,
+    y: game.height / 2,
+    width: 100,
+    height: 120,
+    speed: 10,
+    gap: 10,
+    src: '../images/2.png',
+    gifs: [
+        ['../images/gif/rightMove/', 6, '.png'],
+        ['../images/gif/leftMove/', 6, '.png'],
+    ],
+});
+
+p1.bind('keydown', function (e) {
+    e.preventDefault();
+    switch (e.keyCode) {
+        case 39: //右
+            this.settings.direction = 1;
+            this.startGif();
+            this.startMove();
+            this.turnGif(0);
+            break;
+        case 37: //左
+            this.settings.direction = 3;
+            this.startGif();
+            this.startMove();
+            this.turnGif(1);
+            break;
+    }
+});
+p1.bind('keyup', function (e) {
+    e.preventDefault();
+    switch (e.keyCode) {
+        case 39: //右
+            this.stopGif();
+            this.stopMove();
+            break;
+        case 37: //左
+            this.stopGif();
+            this.stopMove();
+            break;
+    }
+})
+
+stage.createItem({
+    color: '#fce083',
+    x: 50,
+    y: 50,
     width: 50,
     height: 50,
-});
-game.start();
+    speed: 15,
+    src: '../images/1.png',
+    animate: function () {},
+})
+
+game.init();
 
